e856f475f4b1e964ab7e27d1e821f5eaf418a943
SlimFromDub <59626927+SlimFromDub@users.noreply.github.com>
2024-04-23 22:42:45 +0200

    Delete server/controller directory

--- server/controller/attendance.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const UpdateAbl = require("../abl/attendance/updateAbl");
-
-router.post("/update", UpdateAbl);
-
-module.exports = router;
