39d804d5f11e87b7b6be9809409a86f6e2d99e30
SlimFromDub <59626927+SlimFromDub@users.noreply.github.com>
2024-04-24 22:34:18 +0200

    Update listAbl.js

--- server/abl/recipe/listAbl.js
+++ server/abl/recipe/listAbl.js
@@ -1,12 +1,30 @@
 const recipeDao = require("../../dao/recipe-dao.js");
+const cuisineDao = require("../../dao/cuisine-dao.js");
+const alergenDao = require("../../dao/cuisine-dao.js");
+
+const schema = {
+  type: "object",
+  properties: {
+    name: { type: "string"},
+    cuisineId: { type: "string" },
+    ingredientList: {
+                      "type": "object",       
+                      "properties": 
+                      {
+                      name: { type: "string"},
+                      }
+                    }
+                },
+};
 
 async function ListAbl(req, res) {
   try {
     let recipeSearch = req.body;
     
     const recipeList = recipeDao.list();
-    //const passCorr = recipeList.filter((recipeList) recipeList.name);
-    res.json(recipeList);
+    const pecipeFilter = recipeList.find((u)=> (u.name===recipeSearch.name || u.cuisineId===recipeSearch.cuisineId || 
+    u.ingredientList.name===recipeSearch.ingredientList.name));
+    res.json(pecipeFilter);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
